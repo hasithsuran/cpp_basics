23699ebc2f3af2c6247a79585ada658187321029
hasith-suran <hasithsuran@gmail.com>
2022-12-26 21:25:43 +0530

    Added bitwise operator demonstrations

--- include/TC_BitwiseOp.h
+++ include/TC_BitwiseOp.h
@@ -6,9 +6,34 @@ namespace techyB
     class TC_BitwiseOp
     {
     private:
+        //bitshifting varibles
+        unsigned short int shiftNum{0xff0u};
+        unsigned short int rightShiftNum{0xff0u};
+
+        //bitwise logical operands
+        unsigned char logicalOp1 {0x3};
+        unsigned char logicalOp2 {0x5};
+
+        //display formatting params
+        const int COLUMN_WIDTH = 20;
+
+        using VERBOSE_MODE = unsigned char;
+
     public:
+        enum NUMBER_BASE
+        {
+            DECIMAL = 0x1,
+            HEX_DECIMAL = 0x2,
+            BINARY = 0x4,
+            OCTAL = 0x8
+        };
+
         TC_BitwiseOp() {}
-        void performBitwise();
+        void performBitLeftShift();
+        void performBitRightShift();
+        void doLogicalBitwiseOperation();
+        void printNumberInBase(unsigned short int num, NUMBER_BASE base);
+        void printNumMultiMode(unsigned short int num, VERBOSE_MODE verboseMode);
     };
 
 }
